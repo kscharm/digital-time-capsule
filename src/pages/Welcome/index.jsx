34620399459da19822e688f093b8c1c733eab115
Violet Armendariz <varmendari14@gmail.com>
2018-08-27 13:43:50 -0400

    Changed how background was accessed

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -10,7 +10,7 @@ export default class WelcomePage extends Component {
 
   render() {
     return (
-        <div className='bgDiv' >
+        <div className='bgDiv' style={{background: `url(${Background})`}} >
           <div className='welcomeMessage' >
             <span className='mainMessage'> Welcome! </span>
             <span className='subMessage'> This is your hell, and mine. </span>
