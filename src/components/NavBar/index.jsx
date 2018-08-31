126afa3596bbb42559e28fa3d0d64550349c5d31
Violet Armendariz <varmendari14@gmail.com>
2018-08-31 11:40:41 -0400

    Added NavBar component

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const NavBar = (props) => {
+  return (
+    <div className='navBar'>
+        <div className='logo'>
+            <span className='copyright' > © Georgia Institute of Technology </span>
+        </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/pages/PersonalCapsule/index.jsx
+++ src/pages/PersonalCapsule/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import OurButton from '../../components/OurButton';
 import Spinner from '../../components/Spinner';
+import Background from '../../images/cork.jpg';
+import NavBar from '../../components/NavBar';
+import './style.css';
 
 export default class PersonalCapsule extends Component {
   constructor(props) {
@@ -23,11 +26,13 @@ export default class PersonalCapsule extends Component {
   render() {
     return (
         <div id='capsulePage'>
-          <OurButton 
-            buttonAction={() => { this.hello() }} 
-            buttonText='CLICK ME' >
-          </OurButton>
-          <Spinner isLoading={this.state.isLoading} handleLoading={this.handleLoading} />
+          <div className='capsuleDiv' style={{background: `url(${Background})`, backgroundSize:'cover'}} >
+            <OurButton 
+              buttonAction={() => { this.hello() }} 
+              buttonText='CLICK ME' >
+            </OurButton>
+            <Spinner isLoading={this.state.isLoading} handleLoading={this.handleLoading} />
+          </div>
         </div>
     );
   };
