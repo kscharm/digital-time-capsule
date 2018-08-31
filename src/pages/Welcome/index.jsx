87693111cfd2ddac57cd4a3bd88b2d988921f974
stephaniebaione <stephaniebaione@gatech.edu>
2018-08-31 11:36:42 -0400

    made cork board less blurry

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -10,7 +10,7 @@ export default class WelcomePage extends Component {
 
   render() {
     return (
-        <div className='bgDiv' style={{background: `url(${Background})`}} >
+        <div className='bgDiv' style={{background: `url(${Background})`, backgroundSize: 'cover'}} >
           <div className='welcomeMessage' >
             <span className='mainMessage'> Welcome! </span>
             <span className='subMessage'> This is your hell, and mine. </span>
