a4b07de271822d0300d4df35445db227b6f8a08e
Violet Armendariz <varmendari14@gmail.com>
2018-09-01 12:42:33 -0400

    Added proper icons, no drop yet

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 import Search from '../Search'
-import { FaBeer } from 'react-icons/fa';
+import { FaHome, FaCloudUploadAlt, FaUserCircle, FaBars } from 'react-icons/fa';
 
-const NavBar = (props) => {
-  return (
-    <div className='navBar'>
-        <div className='search'>
-            <Search />
-            <FaBeer/>
-        </div>
-    </div>
-  );
+export default class NavBar extends Component {
+  dropMenu = () => {
+    console.log('hello');
+  }
+
+  render() {
+    return (
+      <div className='navBar'>
+          <div className='search'>
+              <Search />
+          </div>
+          <div className='icons'>
+            <FaHome className='icon' onClick={this.dropMenu} />
+            <FaCloudUploadAlt className='icon' onClick={this.dropMenu} />
+            <FaUserCircle className='icon' onClick={this.dropMenu} />
+            <FaBars className='icon' onClick={this.dropMenu} />
+          </div>
+      </div>
+    );
+  }
 };
-
-export default NavBar;
