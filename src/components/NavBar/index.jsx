06a0832dc0448de4b30e468d43ff323695a874f9
Violet Armendariz <varmendari14@gmail.com>
2018-09-01 14:01:27 -0400

    Small Dropdown added

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,24 +3,50 @@ import './style.css';
 import Search from '../Search'
 import { FaHome, FaCloudUploadAlt, FaUserCircle, FaBars } from 'react-icons/fa';
 
+const OurButtons = (props) => {
+  return (
+      <button onClick={props.buttonAction} className={ `our-btn-default our-btn-${props.buttonType}` }>{props.buttonText}</button>
+  );
+};
+
 export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    showDropDown: false,
+  }
+
+  handleLoading = (isLoading) => {
+    this.setState({isLoading: isLoading});
+  }
+
   dropMenu = () => {
     console.log('hello');
+    this.setState({showDropDown: true});
   }
 
   render() {
     return (
-      <div className='navBar'>
-          <div className='search'>
-              <Search />
-          </div>
-          <div className='icons'>
-            <FaHome className='icon' onClick={this.dropMenu} />
-            <FaCloudUploadAlt className='icon' onClick={this.dropMenu} />
-            <FaUserCircle className='icon' onClick={this.dropMenu} />
-            <FaBars className='icon' onClick={this.dropMenu} />
-          </div>
+      <div>
+        <div className='navBar'>
+            <div className='search'>
+                <Search />
+            </div>
+            <div className='icons'>
+              <FaHome className='icon' onClick={() => { this.dropMenu('home')}} />
+              <FaCloudUploadAlt className='icon' onClick={() => { this.dropMenu('cloud')}} />
+              <FaUserCircle className='icon' onClick={() => { this.dropMenu('user')}} />
+              <div className='dropDown'>
+                <FaBars className='icon' onClick={() => {this.dropMenu('menu')}} />
+                <div className='dropDown-content'>
+                  {this.state.showDropDown ? <OurButtons/> : null}
+                </div>
+              </div>
+            </div>
+        </div>
       </div>
     );
-  }
-};
+  };
+}
