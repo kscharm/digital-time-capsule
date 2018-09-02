9e6c40f1617a3a7c03ac067e2e8b40947f899f42
stephaniebaione <stephaniebaione@gatech.edu>
2018-09-01 22:46:58 -0400

    styled add to match navBar. Popup to be done later

---
+++ src/components/AddButton/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+import {FaPlus} from 'react-icons/fa';
+
+const AddButton = (props) => {
+  return (
+      <div className='add'>     
+        <button onClick={props.buttonAction} className={ `our-btn-default our-btn-${props.buttonType}` }>
+        <FaPlus className='addIcon' size={32}/>
+        </button>
+      </div>
+  );
+};
+
+export default AddButton;
