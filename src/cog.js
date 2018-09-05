1d86b3189a9f2378ca6f0f061303ae3008eb60f7
Kenny Scharm <kscharm3@gatech.edu>
2018-09-05 13:21:16 -0400

    Fix mongo error

--- src/cog.js
+++ src/cog.js
@@ -1,10 +1,12 @@
+/*
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/capsule";
 
-export default function mongo() {
+export default function createDatabase() {
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     console.log("Database created!");
     db.close();
   });
 }
+*/
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import cog from './cog';
 
 // Control for redering the app
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-cog();
