40b0452e9d98fa054b3bba5f12267b3cf2a9185e
ptoms <ptoms3@gatech.edu>
2018-09-05 11:59:31 -0400

    added export to the cog

--- src/cog.js
+++ src/cog.js
@@ -1,8 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/capsule";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
+export default function mongo() {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+  });
+}
