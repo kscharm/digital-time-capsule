6a54853352268986ed32b874a8ba504e4167ec4c
ptoms <ptoms3@gatech.edu>
2018-09-05 11:50:08 -0400

    Should connect mongo to the server start up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import cog from './cog';
 
 // Control for redering the app
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+cog();
