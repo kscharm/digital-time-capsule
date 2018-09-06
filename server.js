cc00276eabbb0f599e409d4c85f709576a35a194
Kenny Scharm <kscharm3@gatech.edu>
2018-09-06 16:28:37 -0400

    Move mongo connect to correct location

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+const url = "mongodb://localhost:27017/capsule";
 
 app.set("port", process.env.PORT || 3001);
 
@@ -7,7 +9,12 @@ app.set("port", process.env.PORT || 3001);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+// eslint-disable no-console
 app.listen(app.get("port"), () => {
-  console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+  console.log(`Find the server at: http://localhost:${app.get("port")}/`);
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+  });
 });
