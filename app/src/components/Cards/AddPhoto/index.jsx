c13949f670c987450641429640a497182f61bd7b
Violet Armendariz <varmendari14@gmail.com>
2018-09-08 21:56:14 -0400

    Final layout additions, NO FUNC

--- app/src/components/Cards/AddPhoto/index.jsx
+++ app/src/components/Cards/AddPhoto/index.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import './style.css';
 import '../../OurButton';
-import { 
-  FaImage,
-} from 'react-icons/fa';
+import addPhotoBase from '../../../images/addPhoto.png'
 import OurButton from '../../OurButton';
 
 export default class AddPhoto extends Component {
@@ -20,16 +18,38 @@ export default class AddPhoto extends Component {
     <div className='addPhoto'>
         <div className='addPhotoBack'/>
         <div className='addPhotoCard'>
-            <OurButton
-                buttonText='Add'
-                buttonAction={() => {this.closeAddPhoto()}}
-                buttonType='primary'
-            />
-            <OurButton
-                buttonText='Cancel'
-                buttonAction={() => {this.closeAddPhoto()}}
-                buttonType='secondary'
-            />
+            {/* <span className='sectionLabels'> Add Photo: </span> */}
+            <div className='photo'>
+                <img className="photoImg" src={addPhotoBase} alt="" />
+            </div>
+            <div className='info'>
+                <div>
+                    Click the icon to open file explorer,
+                </div>
+                <div>
+                    or drag a photo to add.
+                </div>
+            </div>
+            <span className='sectionLabels'> Add Caption: </span>
+            <input id='caption' placeholder='Caption...' style={{marginBottom: '1em'}}/>
+            <span className='sectionLabels'> Choose Frame: </span>
+            <div>
+                <img className="frameImg" src={addPhotoBase} alt="" />
+                <img className="frameImg" src={addPhotoBase} alt="" />
+                <img className="frameImg" src={addPhotoBase} alt="" />
+            </div>
+            <div className='actionButtons'>
+                <OurButton
+                    buttonText='Add'
+                    buttonAction={() => {this.closeAddPhoto()}}
+                    buttonType='primary'
+                />
+                <OurButton
+                    buttonText='Cancel'
+                    buttonAction={() => {this.closeAddPhoto()}}
+                    buttonType='secondary'
+                />
+            </div>
         </div>
     </div>
     );
