64c0ef1f4ba5db3b617639cc6a8033070dac96f0
Violet Armendariz <varmendari14@gmail.com>
2018-09-11 18:55:10 -0400

    Added base textbox

--- app/src/components/Cards/AddText/index.jsx
+++ app/src/components/Cards/AddText/index.jsx
@@ -3,22 +3,58 @@ import './style.css';
 import '../../OurButton';
 import addPhotoBase from '../../../images/addPhoto.png'
 import OurButton from '../../OurButton';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 export default class AddText extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  closeAddText = () => {
-    this.props.handleShowAddText(false);
-  }
+    state = {
+        editorState: EditorState.createEmpty()
+    }
+
+    closeAddText = () => {
+        this.props.handleShowAddText(false);
+    }
+
+    // The following methods are for The text editor
+    onChange = editorState => {this.setState({editorState})};
+    handleKeyCommand(command, editorState) {
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+        if (newState) {
+        this.onChange(newState);
+        return 'handled';
+        }
+        return 'not-handled';
+    }
+    // When a user clicks a button, Change the style
+    _onClick = (e) => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, e.target.name));
+    }
+
 
   render() {
+    // A list of possible styles that conforms with RichUtils abilities
+    const styles = ['BOLD', 'ITALIC', 'UNDERLINE', 'CODE'];
+    const buttons = styles.map(style => {
+      return <button key={style} onClick={this._onClick} name={style}>{style}</button>
+    })
     return (
     <div className='addText'>
         <div className='addTextBack'/>
         <div className='addTextCard'>
             {/* <span className='sectionLabels'> Add Photo: </span> */}
+            <div className='textDiv'>
+                <div className='toolbar'>
+                    {buttons}
+                </div>
+                <Editor 
+                    editorState={this.state.editorState}
+                    handleKeyCommand={this.handleKeyCommand}
+                    onChange={this.onChange}
+                />
+            </div>
             <div className='actionButtons'>
                 <OurButton
                     buttonText='Add'
