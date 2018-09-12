79da3825c53eae33e0677f7e870b01a92a1be611
stephaniebaione <stephaniebaione@gatech.edu>
2018-09-11 20:41:38 -0400

    made these changes to get the branch to appear

--- app/src/components/Cards/AddPhoto/index.jsx
+++ app/src/components/Cards/AddPhoto/index.jsx
@@ -4,23 +4,66 @@ import '../../OurButton';
 import addPhotoBase from '../../../images/addPhoto.png'
 import OurButton from '../../OurButton';
 
+import ImageUploader from 'react-images-upload';
+
 export default class AddPhoto extends Component {
   constructor(props) {
     super(props);
+    this.state = { pictures: [addPhotoBase] };
+    this.onDrop = this.onDrop.bind(this);
+    console.log(this.state.pictures);
+  }
+
+  state = {
+    filePop: false,
+  }
+
+  handleFilePop = (pop) => {
+    this.setState({filePop: pop});
   }
 
   closeAddPhoto = () => {
     this.props.handleShowAddPhoto(false);
   }
 
+  onDrop(picture) {
+    console.log(this.state.pictures);
+    this.setState({
+        pictures: this.state.pictures = picture,
+    });
+    console.log(this.state.pictures);
+}
+
   render() {
+      const FileExplorer = (/*props*/) => {
+        return (
+            <div>
+              <ImageUploader
+                withIcon={true}
+                buttonText='Choose file'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
+            </div>
+        );
+      }
     return (
     <div className='addPhoto'>
         <div className='addPhotoBack'/>
         <div className='addPhotoCard'>
             {/* <span className='sectionLabels'> Add Photo: </span> */}
             <div className='photo'>
-                <img className="photoImg" src={addPhotoBase} alt="" />
+                {/* <img className="photoImg" src={this.state.pictures} alt=""/> */}
+                <div>
+                    <ImageUploader
+                        withIcon={true}
+                        buttonText='Choose file'
+                        onChange={this.onDrop}
+                        imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                        maxFileSize={5242880}
+                    />
+                </div>
             </div>
             <div className='info'>
                 <div>
@@ -29,6 +72,13 @@ export default class AddPhoto extends Component {
                 <div>
                     or drag a photo to add.
                 </div>
+                {/* <div className="fileButton" style={{paddingLeft: '37%', paddingRight: '37%'}}>
+                    <OurButton
+                        buttonText='Upload File'
+                        buttonAction={() => {this.handleFilePop(!this.state.filePop)}}
+                        buttonType='primary'
+                    />
+                </div> */}
             </div>
             <span className='sectionLabels'> Add Caption: </span>
             <input className='caption' placeholder='Caption...' style={{marginBottom: '1em'}}/>
@@ -41,7 +91,7 @@ export default class AddPhoto extends Component {
             <div className='actionButtons'>
                 <OurButton
                     buttonText='Add'
-                    buttonAction={() => {this.closeAddPhoto()}}
+                    buttonAction={() => {this.closeAddPhoto}}
                     buttonType='primary'
                 />
                 <OurButton
@@ -50,6 +100,9 @@ export default class AddPhoto extends Component {
                     buttonType='secondary'
                 />
             </div>
+        </div>
+        <div className='filePop'  style={this.state.filePop ? {display: 'block', position: 'absolute'} : {display: 'none'}}>
+            {this.state.filePop ? <FileExplorer/> : null}
         </div>
     </div>
     );
