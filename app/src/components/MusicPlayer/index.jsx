895cc4df4985510ce3601c27926b1281f9c175fd
Violet Armendariz <varmendari14@gmail.com>
2018-09-21 23:00:11 -0400

    Moved music decode to proper file

--- app/src/components/MusicPlayer/index.jsx
+++ app/src/components/MusicPlayer/index.jsx
@@ -16,6 +16,20 @@ export default class MusicPlayer extends Component {
         position: 0,
     }
 
+    urltoFile = (url, filename, mimeType) => {
+        return (fetch(url)
+            .then(function(res){return res.arrayBuffer();})
+            .then(function(buf){return new File([buf], filename, {type:mimeType});})
+        );
+    }
+
+    // this.urltoFile('data:audio/mp3;base64,aGVsbG8gd29ybGQ=', this.props.song, 'audio/mp3')
+    // .then((file) => {
+    //     console.log('hello');
+    //     console.log(file);
+    //     console.log('goodbye');
+    // })
+
     render () {
         const w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
         console.log(w);
