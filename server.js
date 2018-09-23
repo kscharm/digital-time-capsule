38cefd696fcac49bd44024f8423e1e6c3572b6ed
ptoms <ptoms3@gatech.edu>
2018-09-23 11:47:04 -0400

    Fixed res on adding photos

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.post('/photo', (req, res) => {
     if (err) {
       res.status(500).send({error: err.message});
     } else {
-      res.sendStatus(200);
+      res.status(200).send(photos);
     }
   })
 })
