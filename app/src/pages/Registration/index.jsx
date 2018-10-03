fe82d924968cf7c0dccbf98f8f07b56d762258e1
stephaniebaione <stephaniebaione@gatech.edu>
2018-10-03 15:28:26 -0400

    edited registration page style. bugged

--- app/src/pages/Registration/index.jsx
+++ app/src/pages/Registration/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactLink from '../../components/ReactLink';
 import Background from '../../images/cork.jpg';
 import './style.css';
 
@@ -15,57 +14,54 @@ export default class Registration extends Component {
   render() {
     console.log('hello');
     return (
-        <div className='bgDiv' style={{background: `url(${Background})`, backgroundSize: 'cover'}} >
-          <div className='welcomeMessage' >
-            <span className='mainMessage'> Registration! </span>
-            <span className='subMessage'> Input your information below! </span>
-          </div>
-          <div className='register'>
-            <div className="registrationForm">
-              <form action={'/currentCapsule'} target="_self">
-                <label>
-                  First Name:
-                  <input type="text" name="email" />
-                </label>
-                <div></div>
-                <label>
-                  Last Name:
-                  <input type="text" name="email" />
-                </label>
-                <div></div>
-                <label>
-                  Major:
-                  <input type="text" name="email" />
-                </label>
-                <div></div>
-                <label>
-                  Minor:
-                  <input type="text" name="email" />
-                </label>
-                <div></div>
-                <label>
-                  Email:
-                  <input type="text" name="email" />
-                </label>
-                <div></div>
-                <label>
-                  Password:
-                  <input type="password" name="password" />
-                </label>
-                <div></div>
-                <label>
-                  Confirm Password:
-                  <input type="password" name="password" />
-                </label>
-                <div></div>
-                <label>
-                  <input type="submit" name="submit" action={'/currentCapsule'}></input>
-                </label>
-              </form>
-            </div>
-          </div>
+      <div className='bgDiv' style={{background: `url(${Background})`, backgroundSize: 'cover'}} >
+        {/* <div className='welcomeMessage' >
+          <span className='mainMessage'> Registration! </span>
+          <span className='subMessage'> Input your information below! </span>
+        </div> */}
+        <div className='register'>
+          <form action="#" method="post">
+          <h2>Registration</h2>
+              <p>
+                <label for="firstName" class="floatLabel">First Name</label>
+                <input id="firstName" name="firstName" type="text"></input>
+              </p>
+              <p>
+                <label for="lastName" class="floatLabel">Last Name</label>
+                <input id="lastName" name="lastName" type="text"></input>
+              </p>
+              <p>
+                <label for="email" class="floatLabel">Email</label>
+                <input id="Email" name="Email" type="text"></input>
+              </p>
+              <p>
+                <label for="university" class="floatLabel">University</label>
+                <input id="university" name="university" type="text"></input>
+              </p>
+              <p>
+                <label for="major" class="floatLabel">Major</label>
+                <input id="major" name="major" type="text"></input>
+              </p>
+              <p>
+                <label for="userID" class="floatLabel">Username</label>
+                <input id="userId" name="userId" type="text"></input>
+              </p>
+              <p>
+                <label for="password" class="floatLabel">Password</label>
+                <input id="password" name="password" type="password"></input>
+                <span>Enter a password longer than 8 characters.</span>
+              </p>
+              <p>
+                <label for="confirm_password" class="floatLabel">Confirm Password</label>
+                <input id="confirm_password" name="confirm_password" type="password"></input>
+                <span>Your passwords do not match.</span>
+              </p>
+              <p>
+                <input type="submit" value="Create My Account" id="submit"></input>
+              </p>
+          </form>
         </div>
-        
+      </div>
     );
   };
 }
