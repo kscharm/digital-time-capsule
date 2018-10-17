6bbe96fa8aca36afb0d452bbb6859c5b770e2846
Violet Armendariz <varmendari14@gmail.com>
2018-10-17 10:27:33 -0400

    Login routing works

--- app/src/pages/Welcome/index.jsx
+++ app/src/pages/Welcome/index.jsx
@@ -5,6 +5,15 @@ import { withRouter, Redirect } from 'react-router-dom';
 import axios from 'axios';
 import bcrypt from 'bcryptjs';
 
+/* A REAL authentication function */
+export const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated=true;
+    setTimeout(cb, 100);
+  }
+};
+
 class Login extends React.Component {
 
   constructor() {
@@ -17,9 +26,22 @@ class Login extends React.Component {
     pass: '',
   }
   login = ()=>{
-    fakeAuth.authenticate(() => {
-      this.setState({ redirectToReferrer: true })
-    })
+    axios.get('http://localhost:3001/validateUser?username=' + this.state.user)
+      .then((res) => {
+        const user = res.data;
+        bcrypt.compare(this.state.pass, user.password)
+          .then((res) => {
+            fakeAuth.authenticate(() => {
+              this.setState({ redirectToReferrer: true })
+            })
+          })
+          .catch((err) => {
+            alert('Invalid username and password combination');
+          });
+        })
+      .catch((err) => {
+          alert('Error validating user: ' + err.message);
+      });
   }
   updateUsername = (evt) => {
     this.setState({user: evt.target.value})
@@ -38,64 +60,40 @@ class Login extends React.Component {
       )
     }
 
-    // return (
-    //     <div className='bgDiv' style={{background: `url(${Background})`, backgroundSize: 'cover'}} >
-    //       <div className='login'>
-    //         <form className="login-form" target="_self">
-    //           <div className='welcomeMessage'>
-    //             <span className='mainMessage'> Welcome! </span>
-    //             <span className='subMessage'> Go Jackets! </span>
-    //           </div>
-    //           <ul>
-    //             <li>
-    //                 <label htmlFor="user"> Username</label>
-    //                 <input type="text" id='user' name="user" maxLength="100" onChange={evt => this.updateUsername(evt)}/>
-    //             </li>
-    //             <li>
-    //               <label htmlFor="pass"> Password</label>
-    //               <input type="password" id='pass' name="pass" maxLength="100" onChange={evt => this.updatePass(evt)}/>
-    //             </li>
-    //           </ul>
-    //           <button onClick={this.login}>Login</button>
-    //           <p className="message">Not registered? <a href="/registration">Create an account</a></p>
-    //         </form>
-    //       </div>
-    //     </div>
-        
-    // );
     return (
-      <div>
-        <p>You must log in to view the page at {from.pathname}</p>
-        <button onClick={this.login}>Log in</button>
-      </div>
-    )
+        <div className='bgDiv' style={{background: `url(${Background})`, backgroundSize: 'cover'}} >
+          <div className='login'>
+            <div className="login-form">
+              <div className='welcomeMessage'>
+                <span className='mainMessage'> Welcome! </span>
+                <span className='subMessage'> Go Jackets! </span>
+              </div>
+              <ul>
+                <li>
+                    <label htmlFor="user"> Username</label>
+                    <input type="text" id='user' name="user" maxLength="100" onChange={evt => this.updateUsername(evt)}/>
+                </li>
+                <li>
+                  <label htmlFor="pass"> Password</label>
+                  <input type="password" id='pass' name="pass" maxLength="100" onChange={evt => this.updatePass(evt)}/>
+                </li>
+              </ul>
+              <button onClick={this.login}>Login</button>
+              <p className="message">Not registered? <a href="/registration">Create an account</a></p>
+            </div>
+          </div>
+        </div>
+        
+    );
+    // return (
+    //   <div>
+    //     <p>You must log in to view the page at {from.pathname}</p>
+    //     <button onClick={this.login}>Log in</button>
+    //   </div>
+    // )
   }
 
 
 }
-
-/* A fake authentication function */
-export const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    // axios.get('http://localhost:3001/validateUser?username=' + this.state.user)
-    //   .then((res) => {
-    //     const user = res.data;
-    //     bcrypt.compare(this.state.pass, user.password)
-    //       .then((res) => {
-    //         this.isAuthenticated=true;
-    //       })
-    //       .catch((err) => {
-    //         alert('Invalid username and password combination');
-    //       });
-    //     })
-    //   .catch((err) => {
-    //       alert('Error validating user: ' + err.message);
-    //   });
-    this.isAuthenticated=true;
-    setTimeout(cb, 100);
-  }
-};
-
 
 export default Login
