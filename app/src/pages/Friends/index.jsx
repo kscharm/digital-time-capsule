b9813d45d3b2967ab66d6cf5cb1b1fe0103bb01d
Violet Armendariz <varmendari14@gmail.com>
2018-10-20 16:54:42 -0400

    Added Background color

--- app/src/pages/Friends/index.jsx
+++ app/src/pages/Friends/index.jsx
@@ -24,19 +24,20 @@ export default class Friends extends Component {
 
     return (
       <div className='bgDiv' style={{background: `url(${Background})`, overflow:'auto'}} >
-      <NavBar handlePop={this.handlePop} addPop={this.state.addPop} />
-      <div className='addButton'>
-            {/* <AddButton
-              buttonAction={() => { this.handlePop(!this.state.addPop) }}
-              buttonType='add'
-            /> */}
-        </div>
-        <h2 style={{margin: '7% 0 0 3%'}}>Friends</h2>
-        <div>
-          <img src={toBeCapsule} alt='placeholder' style={{zoom: '50%', padding: '20px 30px 0px 20px'}}></img>
-          <img src={toBeCapsule} alt='placeholder' style={{zoom: '50%', padding: '20px 30px 0px 20px'}}></img>
-          <img src={toBeCapsule} alt='placeholder' style={{zoom: '50%', padding: '20px 30px 0px 20px'}}></img>
-        </div>
+      <div>
+          <div className={ `bkgOverlay` }/>
+          <div className={ `capsuless` }>
+            <NavBar handlePop={this.handlePop} addPop={this.state.addPop} />
+            <div className='addButton'>
+              </div>
+              <h2 style={{margin: '7% 0 0 3%'}}>Friends</h2>
+              <div>
+                <img src={toBeCapsule} alt='placeholder' style={{zoom: '50%', padding: '20px 30px 0px 20px'}}></img>
+                <img src={toBeCapsule} alt='placeholder' style={{zoom: '50%', padding: '20px 30px 0px 20px'}}></img>
+                <img src={toBeCapsule} alt='placeholder' style={{zoom: '50%', padding: '20px 30px 0px 20px'}}></img>
+              </div>
+            </div>
+          </div>
       </div>
     );
   };
