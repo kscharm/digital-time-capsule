e4433e635a554a4d1d4a01bc3d657bc51e66731c
stephaniebaione <stephaniebaione@gatech.edu>
2018-11-01 01:34:49 -0400

    created userDisplay

---
+++ app/src/components/UserDisplay/index.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './style.css';
+import {
+    FaTrash
+} from 'react-icons/fa';
+
+import toBeCapsule from '../../images/addPhoto.png';
+import AddButton from '../../components/AddButton';
+
+export default class UserDisplay extends Component {
+    // constructor(props) {
+    //   super(props)
+    // }
+
+    render () {
+        const DeleteButton = () => {
+            return (
+            <button
+                onClick={() => {console.log('We click a delete.')}}
+                className='deleteButton'
+            >
+                <FaTrash className='deleteIcon' size={20}/>
+            </button>
+            )}
+        return (
+            <div className='userDisplayGroup'>
+            <div className='userPiny'></div>
+            <div className={`userDisplay`}>
+                {/* <div className='userPin'></div> */}
+                <div className="title-content">
+                    <h3>{this.props.title}</h3>
+                    <hr />
+                    {/* <div className="intro">{this.props.id}</div> */}
+                </div>
+                <div className='addButton' style={{position: 'absolute;', marginLeft: '19%;'}}>
+                    <AddButton
+                        buttonAction={() => {console.log("add friend button clicked!")}}
+                        buttonType='add'
+                        style={{position: 'absolute;', marginLeft: '19%;'}}
+                    />
+                    </div>
+                {/* <div className="card-info">{this.props.description}</div> */}
+                <div>
+                <img src={toBeCapsule} alt='placeholder' style={{zoom: '50%', padding: '20px 30px 0px 20px'}}></img>
+                </div>
+
+                {this.props.showDelete ? <DeleteButton/> : null}
+            </div>
+            </div>
+        );
+    }
+}
