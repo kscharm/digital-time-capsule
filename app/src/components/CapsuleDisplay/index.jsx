9c22a77eaee52a36a0bf0963c26277d5c3a3bab9
Violet Armendariz <varmendari14@gmail.com>
2018-11-02 13:52:59 -0400

    You can only see add and remove if u dont own, arent in capsule

--- app/src/components/CapsuleDisplay/index.jsx
+++ app/src/components/CapsuleDisplay/index.jsx
@@ -1,20 +1,59 @@
 import React, { Component } from 'react';
 import './style.css';
 import {FaPlus, FaMinus} from 'react-icons/fa';
+import axios from 'axios';
 
 export default class CapsuleDisplay extends Component {
     // constructor(props) {
     //   super(props)
     // }
+    state = {
+        isOwner: false,
+        isContributer: false,
+    }
     requestCapsuleAccess = () => {
         console.log('I should req access for user and capsule:');
         console.log(this.props.userID);
         console.log(this.props.id);
+        console.log(this.props.capsuleOwner);
+        console.log(this.props.capsuleContributer);
     }
     requestCapsuleRemoval = () => {
         console.log('I should req removal for user and capsule:');
         console.log(this.props.userID);
         console.log(this.props.id);
+    }
+    checkCapsuleOwner = (capsuleId, id) => {
+        axios.get('http://localhost:3001/capsuleOwner?capsule=' + capsuleId)
+          .then((res) => {
+            // Add each type to their respective arrays
+            if (res.data === id) {
+              this.setState({isOwner: true});
+            }
+          })
+          .catch((err) => {
+              alert('Error getting media: ' + err.message);
+          });
+    }
+    checkCapsuleContributer = (capsuleId, id) => {
+    axios.get('http://localhost:3001/capsuleContributors?capsule=' + capsuleId)
+        .then((res) => {
+        // Add each type to their respective arrays
+        console.log(res.data);
+        for (let i = 0; i < res.data.length; i ++) {
+            if (res.data[i] === id) {
+            this.setState({isContributer: true});
+            break;
+            }
+        }
+        })
+        .catch((err) => {
+            alert('Error getting media: ' + err.message);
+        });
+    }
+    componentDidMount = () => {
+        this.checkCapsuleOwner(this.props.id,this.props.userID);
+        this.checkCapsuleContributer(this.props.id,this.props.userID);
     }
     render () {
         const AddButton = () => {
@@ -48,8 +87,8 @@ export default class CapsuleDisplay extends Component {
                     <div>{this.props.description}</div>
                 </div>
                 <div>
-                <AddButton/>
-                <DeleteButton/>
+                {(this.state.isOwner || this.state.isContributer) ? null : <AddButton/>}
+                {this.state.isOwner ? null : <DeleteButton/>}
                 </div>
             </div>
             </div>
