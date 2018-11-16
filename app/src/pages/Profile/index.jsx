30df34e67dbd1ce1300da35d3837f8d3aec811e8
Violet Armendariz <varmendari14@gmail.com>
2018-11-16 12:31:35 -0500

    Fixed button issues and location of fields

--- app/src/pages/Profile/index.jsx
+++ app/src/pages/Profile/index.jsx
@@ -6,117 +6,10 @@ import axios from 'axios';
 
 import NavBar from '../../components/NavBar';
 
-
-function DisplayUserInfo(props) {
-  return (
-  <div>
-  <h2>User Information (This one is a display)</h2>
-  <ul>
-    <li>
-        <label for="firstName"> First Name</label>
-        <p type="text" id='firstName' name="firstName" style={{fontSize: "13px"}}>Stephanie</p>
-        <span>Your first name</span>
-    </li>
-    <li>
-        <label for="lastName"> Last Name</label>
-        <input type="text" id='lastName' name="lastName" maxLength="100" onChange={evt => this.updateLast(evt)}/>
-        <span>Enter your last name</span>
-    </li>
-    <li>
-        <label for="email"> Email</label>
-        <input type="text" id='email' name="email" maxLength="100" onChange={evt => this.updateEmail(evt)}/>
-        <span>Enter your email</span>
-    </li>
-    <li>
-        <label for="university"> University</label>
-        <input type="text" id='university' name="university" maxLength="100" onChange={evt => this.updateUniversity(evt)}/>
-        <span>Enter your university</span>
-    </li>
-    <li>
-        <label for="major"> Major</label>
-        <input type="text" id='major' name="major" maxLength="100" onChange={evt => this.updateMajor(evt)}/>
-        <span>Enter your major</span>
-    </li>
-    <li>
-        <label for="username"> Username</label>
-        <input type="text" id='username' name="username" maxLength="100" onChange={evt => this.updateUsername(evt)}/>
-        <span>Enter your username</span>
-    </li>
-    <li>
-        <label for="pass1"> Password</label>
-        <input type="password" id='pass1' name="pass1" maxLength="100" onChange={evt => this.updatePass(evt)}/>
-        <span>Enter your password</span>
-    </li>
-    <li>
-        <label for="pass2"> Confirm Password</label>
-        <input type="password" id='pass2' name="pass2" maxLength="100" onChange={evt => this.updateConfirmPass(evt)}/>
-        <span>Confirm your password</span>
-    </li>
-  </ul>
-  <p>
-  <button onClick={() => {this.handleButtonClick()}}>Edit User Info</button>
-  </p>
-  </div>);
-}
-
-function EditableUserInfo(props) {
-  return (
-  <div>
-    <h2>User Information (This one is a form)</h2>
-    <ul>
-      <li>
-          <label for="firstName"> First Name</label>
-          <input type="text" id='firstName' name="firstName" maxLength="100" onChange={evt => this.updateFirst(evt)}/>
-          <span>Enter your first name</span>
-      </li>
-      <li>
-          <label for="lastName"> Last Name</label>
-          <input type="text" id='lastName' name="lastName" maxLength="100" onChange={evt => this.updateLast(evt)}/>
-          <span>Enter your last name</span>
-      </li>
-      <li>
-          <label for="email"> Email</label>
-          <input type="text" id='email' name="email" maxLength="100" onChange={evt => this.updateEmail(evt)}/>
-          <span>Enter your email</span>
-      </li>
-      <li>
-          <label for="university"> University</label>
-          <input type="text" id='university' name="university" maxLength="100" onChange={evt => this.updateUniversity(evt)}/>
-          <span>Enter your university</span>
-      </li>
-      <li>
-          <label for="major"> Major</label>
-          <input type="text" id='major' name="major" maxLength="100" onChange={evt => this.updateMajor(evt)}/>
-          <span>Enter your major</span>
-      </li>
-      <li>
-          <label for="username"> Username</label>
-          <input type="text" id='username' name="username" maxLength="100" onChange={evt => this.updateUsername(evt)}/>
-          <span>Enter your username</span>
-      </li>
-      <li>
-          <label for="pass1"> Password</label>
-          <input type="password" id='pass1' name="pass1" maxLength="100" onChange={evt => this.updatePass(evt)}/>
-          <span>Enter your password</span>
-      </li>
-      <li>
-          <label for="pass2"> Confirm Password</label>
-          <input type="password" id='pass2' name="pass2" maxLength="100" onChange={evt => this.updateConfirmPass(evt)}/>
-          <span>Confirm your password</span>
-      </li>
-    </ul>
-    <p>
-      {/*this says that the function is undefined */}
-    <button onClick={() => {this.handleButtonClick()}}>Save Changes</button> {/*this.saveChanges(),  */}
-    </p>
-    </div>);
-}
-
-
 export default class Profile extends Component {
   state = {
     addPop: false,
-    editing: true,
+    editing: false,
   }
   saveChanges = () => {
     console.log("I should save the changes the user made");
@@ -147,7 +40,113 @@ export default class Profile extends Component {
   }
 
   render() {
+    const DisplayUserInfo = () => {
+      return (
+      <div>
+      <h2>User Information (This one is a display)</h2>
+      <ul>
+        <li>
+            <label for="firstName"> First Name</label>
+            <p type="text" id='firstName' name="firstName" style={{fontSize: "13px"}}>Stephanie</p>
+            <span>Your first name</span>
+        </li>
+        <li>
+            <label for="lastName"> Last Name</label>
+            <input type="text" id='lastName' name="lastName" maxLength="100" onChange={evt => this.updateLast(evt)}/>
+            <span>Enter your last name</span>
+        </li>
+        <li>
+            <label for="email"> Email</label>
+            <input type="text" id='email' name="email" maxLength="100" onChange={evt => this.updateEmail(evt)}/>
+            <span>Enter your email</span>
+        </li>
+        <li>
+            <label for="university"> University</label>
+            <input type="text" id='university' name="university" maxLength="100" onChange={evt => this.updateUniversity(evt)}/>
+            <span>Enter your university</span>
+        </li>
+        <li>
+            <label for="major"> Major</label>
+            <input type="text" id='major' name="major" maxLength="100" onChange={evt => this.updateMajor(evt)}/>
+            <span>Enter your major</span>
+        </li>
+        <li>
+            <label for="username"> Username</label>
+            <input type="text" id='username' name="username" maxLength="100" onChange={evt => this.updateUsername(evt)}/>
+            <span>Enter your username</span>
+        </li>
+        <li>
+            <label for="pass1"> Password</label>
+            <input type="password" id='pass1' name="pass1" maxLength="100" onChange={evt => this.updatePass(evt)}/>
+            <span>Enter your password</span>
+        </li>
+        <li>
+            <label for="pass2"> Confirm Password</label>
+            <input type="password" id='pass2' name="pass2" maxLength="100" onChange={evt => this.updateConfirmPass(evt)}/>
+            <span>Confirm your password</span>
+        </li>
+      </ul>
+      <p>
+      <button onClick={() => {this.handleButtonClick()}}>Edit User Info</button>
+      </p>
+      </div>);
+    }
+    
+    const EditableUserInfo = () => {
+      return (
+      <div>
+        <h2>User Information (This one is a form)</h2>
+        <ul>
+          <li>
+              <label for="firstName"> First Name</label>
+              <input type="text" id='firstName' name="firstName" maxLength="100" onChange={evt => this.updateFirst(evt)}/>
+              <span>Enter your first name</span>
+          </li>
+          <li>
+              <label for="lastName"> Last Name</label>
+              <input type="text" id='lastName' name="lastName" maxLength="100" onChange={evt => this.updateLast(evt)}/>
+              <span>Enter your last name</span>
+          </li>
+          <li>
+              <label for="email"> Email</label>
+              <input type="text" id='email' name="email" maxLength="100" onChange={evt => this.updateEmail(evt)}/>
+              <span>Enter your email</span>
+          </li>
+          <li>
+              <label for="university"> University</label>
+              <input type="text" id='university' name="university" maxLength="100" onChange={evt => this.updateUniversity(evt)}/>
+              <span>Enter your university</span>
+          </li>
+          <li>
+              <label for="major"> Major</label>
+              <input type="text" id='major' name="major" maxLength="100" onChange={evt => this.updateMajor(evt)}/>
+              <span>Enter your major</span>
+          </li>
+          <li>
+              <label for="username"> Username</label>
+              <input type="text" id='username' name="username" maxLength="100" onChange={evt => this.updateUsername(evt)}/>
+              <span>Enter your username</span>
+          </li>
+          <li>
+              <label for="pass1"> Password</label>
+              <input type="password" id='pass1' name="pass1" maxLength="100" onChange={evt => this.updatePass(evt)}/>
+              <span>Enter your password</span>
+          </li>
+          <li>
+              <label for="pass2"> Confirm Password</label>
+              <input type="password" id='pass2' name="pass2" maxLength="100" onChange={evt => this.updateConfirmPass(evt)}/>
+              <span>Confirm your password</span>
+          </li>
+        </ul>
+        <p>
+        <button onClick={() => {this.saveChanges()}}>Save Changes</button>
+        <button onClick={() => {this.handleButtonClick()}}>Cancel</button>
+        </p>
+        </div>);
+    }
+    
     const title1 = 'User Information';
+    
     console.log(this.state.user);
 
     let UserInfo;
@@ -165,9 +164,6 @@ export default class Profile extends Component {
           <div className={ `capsuless_general` }>
               <div className={`notepaper-title_general`} style={{maxWidth: "300px", width: "240px"}}>
                 <p className={`text-title_general`}>{title1}</p>
-              </div>
-              <div className='addButton'>
-              <button onClick={this.handleButtonClick}>Click me!</button>
               </div>
               <div className='register'>
                 {UserInfo}
