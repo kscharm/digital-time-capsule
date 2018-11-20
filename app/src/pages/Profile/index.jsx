7bec7f7b574771c5bec7028231c94e1278e3d755
Violet Armendariz <varmendari14@gmail.com>
2018-11-19 21:00:24 -0500

    Fixed value

--- app/src/pages/Profile/index.jsx
+++ app/src/pages/Profile/index.jsx
@@ -21,13 +21,16 @@ export default class Profile extends Component {
     major: '',
     username: '',
     password: '',
+    pass2: '',
 
     file: '',
+    editing: false,
   }
   saveChanges = () => {
     console.log("I should save the changes the user made");
   }
   updateFirst = (evt) => {
+    console.log('we in here now');
     this.setState({firstName: evt.target.value})
   }
   updateLast = (evt) => {
@@ -54,13 +57,14 @@ export default class Profile extends Component {
   getUserInfo(username) {
     axios.get('http://localhost:3001/getUserByUsername?username=' + username)
       .then((res) => {
-        this.setState({firstname: res.data.firstName});
+        this.setState({firstName: res.data.firstName});
         this.setState({lastName: res.data.lastName});
         this.setState({email: res.data.email});
         this.setState({university: res.data.university});
         this.setState({major: res.data.major});
         this.setState({username: res.data.username});
         this.setState({password: res.data.password});
+        this.setState({password: res.data.pass2});
       })
       .catch((err) => {
           alert('Error getting user data: ' + err.response.data);
@@ -120,7 +124,7 @@ export default class Profile extends Component {
       <ul>
         <li className='viewLi'>
             <label htmlFor="firstName"> First Name</label>
-            <p id='firstName' className='viewP' name="firstName">{this.state.firstname}</p>
+            <p id='firstName' className='viewP' name="firstName">{this.state.firstName}</p>
             <span className='viewSpan'>Your first name</span>
         </li>
         <li className='viewLi'>
@@ -172,45 +176,44 @@ export default class Profile extends Component {
         />
         </div>
         <ul>
-          {/*I would love to have placeholders in these, but for some reason, {this.state.firstname} will not appear when set as a placeholder :( */}
           <li>
-              <label for="firstName"> First Name</label>
-              <input type="text" id='firstName' name="firstName" maxLength="100" onChange={evt => this.updateFirst(evt)}/>
+              <label for="firstName">First Name</label>
+              <input type="text" value={this.state.firstName} id='firstName' name="firstName" maxLength="100" onChange={evt => this.updateFirst(evt)}/>
               <span>Enter your first name</span>
           </li>
           <li>
               <label for="lastName"> Last Name</label>
-              <input type="text" id='lastName' name="lastName" maxLength="100" onChange={evt => this.updateLast(evt)}/>
+              <input type="text" value={this.state.lastName} id='lastName' name="lastName" maxLength="100" onChange={evt => this.updateLast(evt)}/>
               <span>Enter your last name</span>
           </li>
           <li>
               <label for="email"> Email</label>
-              <input type="text" id='email' name="email" maxLength="100" onChange={evt => this.updateEmail(evt)}/>
+              <input type="text" value={this.state.email} id='email' name="email" maxLength="100" onChange={evt => this.updateEmail(evt)}/>
               <span>Enter your email</span>
           </li>
           <li>
               <label for="university"> University</label>
-              <input type="text" id='university' name="university" maxLength="100" onChange={evt => this.updateUniversity(evt)}/>
+              <input type="text" value={this.state.university} id='university' name="university" maxLength="100" onChange={evt => this.updateUniversity(evt)}/>
               <span>Enter your university</span>
           </li>
           <li>
               <label for="major"> Major</label>
-              <input type="text" id='major' name="major" maxLength="100" onChange={evt => this.updateMajor(evt)}/>
+              <input type="text" value={this.state.major} id='major' name="major" maxLength="100" onChange={evt => this.updateMajor(evt)}/>
               <span>Enter your major</span>
           </li>
           <li>
               <label for="username"> Username</label>
-              <input type="text" id='username' name="username" maxLength="100" onChange={evt => this.updateUsername(evt)}/>
+              <input type="text" value={this.state.username} id='username' name="username" maxLength="100" onChange={evt => this.updateUsername(evt)}/>
               <span>Enter your username</span>
           </li>
           <li>
               <label for="pass1"> Password</label>
-              <input type="password" id='pass1' name="pass1" maxLength="100" onChange={evt => this.updatePass(evt)}/>
+              <input type="password" value={this.state.password} id='pass1' name="pass1" maxLength="100" onChange={evt => this.updatePass(evt)}/>
               <span>Enter your password</span>
           </li>
           <li>
               <label for="pass2"> Confirm Password</label>
-              <input type="password" id='pass2' name="pass2" maxLength="100" onChange={evt => this.updateConfirmPass(evt)}/>
+              <input type="password" value={this.state.pass2} id='pass2' name="pass2" maxLength="100" onChange={evt => this.updateConfirmPass(evt)}/>
               <span>Confirm your password</span>
           </li>
         </ul>
