a3bfa3496f727cd4e14cc6d3826237921f2a5394
Violet Armendariz <varmendari14@gmail.com>
2018-11-20 13:59:56 -0500

    Fixed defaults

--- app/src/pages/Registration/index.jsx
+++ app/src/pages/Registration/index.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import uuidv4 from 'uuid/v4';
 import bcrypt from 'bcryptjs';
 import addPhoto from '../../images/addPhoto.png'
+import BasePhoto from '../../images/addPhoto.png';
 
 export default class Registration extends Component {
   // constructor(props) {
@@ -28,8 +29,8 @@ export default class Registration extends Component {
     receivedRequests: [],
     settings: {
       privacy: "private",
-      backgroundImage: "",
-      siteColor: ""
+      backgroundImage: Background,
+      siteColor: "#003057"
     }
   }
 
@@ -55,7 +56,7 @@ export default class Registration extends Component {
           // Delete unnecessary fields
           delete doc.toCapsule;
           delete doc.confirmPass;
-          doc.photo = addPhoto;
+          doc.photo = BasePhoto;
           // Add personal time capsule id to list of capsules
           // doc.capsules.push(doc._id);
           axios.post('http://localhost:3001/registerUser', doc)
